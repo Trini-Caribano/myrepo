d74f7193ae4ec4ffff43f5348a4f4f20fcf0cecb
Trini-Caribano <brandon.joseph3@my.uwi.edu>
2020-01-15 14:54:58 -0400

    added project files

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,1 +1,28 @@
 //my file
+#include<iostream>
+#include<fstream>
+#include<string>
+//int main() {
+//	std::ofstream outputFile;
+//	outputFile.open("example file.txt");
+//	outputFile << "I can write to a file! \n";
+//	outputFile << "This is interesting :D";
+//	outputFile.close();
+
+int main() {
+	std::ifstream input_file;
+	std::string filename = "commaSeparatedValues.txt";
+	input_file.open(filename);
+	if (!input_file) {
+		std::cout << "Uh-oh, file not found. \
+			Ensure file is in project directory" << std::endl;
+	}
+	else {
+		std::string fileData = "";
+		while (getline(input_file, fileData, ',')) {
+			std::cout << fileData << std::endl;
+		}
+		input_file.close();
+		return 0;
+	}
+}
